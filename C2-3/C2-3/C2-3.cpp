dd42e931d270fc554300490a69a86a3756f5d8ef
wangold4w@gmail.com <wangold4w@gmail.com>
2020-11-23 00:48:33 +0800

    add return

--- C2-3/C2-3/C2-3.cpp
+++ C2-3/C2-3/C2-3.cpp
@@ -96,7 +96,7 @@ int main()
     // double indirect value : 1024
     // triple indirect value : 1024
 
-
+    return 0;
 }
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
